f2679f7423c0af0ecbb0ee0645adae663ad592da
akjmalamin7 <thpi205526@gmail.com>
2025-04-15 23:33:19 +0600

    update

--- src/controllers/brand/brandController.js
+++ src/controllers/brand/brandController.js
@@ -1,2 +1,6 @@
+const { brandListService } = require("../../services/brand/brandService")
+
 exports.createBrand = async(req,res)=>{}
-exports.brandList = async(req,res)=>{}
+exports.brandList = async(req,res)=>{
+  await brandListService(req,res)
+}
--- src/services/brand/brandService.js
+++ src/services/brand/brandService.js
@@ -1,4 +1,11 @@
 const brandModel = require("../../models/brand/brandModel")
 
-const brandListService = async(req,res)=>{}
+const brandListService = async(req,res)=>{
+  try{
+      let brands = await brandModel.find({});
+     return res.status(200).json({status:"success",message:"Brand data successfully retrieve",data:brands})
+  }catch(err){
+    return res.status(400).json({status:"fail",message:"Brand data not retrieve",data:[]})
+  }
+}
 module.exports = {brandListService}
